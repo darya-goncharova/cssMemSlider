7421d86e6bf792139ee38affb14b9d69b715f2ac
darya goncharova <darya.goncharova23@gmail.com>
2022-05-13 01:21:32 +0300

    feat: add slider animation Fri, May 13, 2022 1:21:21 AM)

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-let counter = 1;
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if (counter > 5) {
-        counter = 1;
-    }
-}, 2000);
+const 
+  slides = document.querySelectorAll('.slide'),
+  dots = document.querySelectorAll('.dot');
+
+let index = 0; // текущий активный слайд
+
+const activeSlide = (n) => {
+  for (slide of slides) {
+    slide.classList.remove('slide_active');
+  }
+  slides[n].classList.add('slide_active');
+};
+const activeDot = (n) => {
+  for (dot of dots) {
+    dot.classList.remove('active');
+  }
+  dots[n].classList.add('active');
+};
+const prepareCurrentSlide = (ind) => {
+  activeSlide(ind);
+  activeDot(ind);
+};
+
+dots.forEach((item, indexDot) => {
+  item.addEventListener('click', () => {
+    index = indexDot;
+    prepareCurrentSlide(index);
+  });
+});
